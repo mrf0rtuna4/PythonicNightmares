09d581d17982ce926921148e40993486079e1155
mrf0rtuna4 <majned90@gmail.com>
2024-09-27 15:37:04 +0300

    чихнул

--- nightmares/scripts/loader.py
+++ nightmares/scripts/loader.py
@@ -7,7 +7,7 @@ def load_all_modules_from_package(package_name):
     package_path = package.__path__[0]
     
     for module_name in os.listdir(package_path):
-        if module_name.endswith(".py") and module_name != "__init__.py":
+        if module_name.endswith(".py") and module_name != "__init__.py" and module_name != "loader.py":
             module_name = module_name[:-3]
             full_module_name = f"{package_name}.{module_name}"
             importlib.import_module(full_module_name)
