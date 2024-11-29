c5f449c75277b20d58b7bbe42fd1a2e329801d74
mrf0rtuna4 <majned90@gmail.com>
2024-11-29 20:21:03 +0300

    исправил прах деда

--- nightmares/modules/loader.py
+++ nightmares/modules/loader.py
@@ -3,11 +3,14 @@ import os
 import sys
 
 def load_modules(package_name):
+    if package_name not in sys.modules:
+        importlib.import_module(package_name)
+    
     package = sys.modules[package_name]
     package_path = package.__path__[0]
-    
+
     for module_name in os.listdir(package_path):
-        if module_name.endswith(".py") and module_name != "__init__.py" and module_name != "loader.py":
+        if module_name.endswith(".py") and module_name not in {"__init__.py", "loader.py"}:
             module_name = module_name[:-3]
             full_module_name = f"{package_name}.{module_name}"
             importlib.import_module(full_module_name)
